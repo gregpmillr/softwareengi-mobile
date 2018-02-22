cc2a045a4a10b796f2396526f37438f246609eae
Ryan Amaral <ryan_amaral@live.com>
2018-02-22 14:18:02 -0400

    tests passed

--- app/src/androidTest/java/com/softengi/mobcomp/softwareengi_mobile/LoginInstrumentedTest.java
+++ app/src/androidTest/java/com/softengi/mobcomp/softwareengi_mobile/LoginInstrumentedTest.java
@@ -2,11 +2,22 @@ package com.softengi.mobcomp.softwareengi_mobile;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.xml.validation.Validator;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,11 +28,25 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class LoginInstrumentedTest {
 
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
 
-        assertEquals("com.softengi.mobcomp.softwareengi_mobile", appContext.getPackageName());
+    @Test
+    public void validateGoodPassword() throws Exception {
+        String user = "bob";
+        String password = "pass";
+        // type user
+        onView(withId(R.id.etUsername))
+                .perform(typeText(user), closeSoftKeyboard());
+        // type password
+        onView(withId(R.id.etPassword))
+                .perform(typeText(password), closeSoftKeyboard());
+        // press login button
+        onView(withId(R.id.btnLogin))
+                .perform(click());
+        // check user text
+        onView(withId(R.id.tvUsername))
+                .check(matches(withText(user)));
     }
 }
