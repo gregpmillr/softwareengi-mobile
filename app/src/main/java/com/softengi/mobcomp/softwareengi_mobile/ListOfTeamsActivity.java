a1b028f5e9885bef9ed057d8cd81b7620a91918c
Ryan Amaral <ryan_amaral@live.com>
2018-03-14 12:55:26 -0300

    updated format of team list view

--- app/src/main/java/com/softengi/mobcomp/softwareengi_mobile/ListOfTeamsActivity.java
+++ app/src/main/java/com/softengi/mobcomp/softwareengi_mobile/ListOfTeamsActivity.java
@@ -2,12 +2,16 @@ package com.softengi.mobcomp.softwareengi_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListOfTeamsActivity extends AppCompatActivity {
 
-    ListView teamListView;
+    private ListView teamListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,14 @@ public class ListOfTeamsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_of_teams);
 
         teamListView = findViewById(R.id.teamListView);
-        String[] teamNames = new String[]{"Team 1", "Team 2"};
+        String[] teamNames = new String[]{"Team 1\n324324", "Team 2\n98437532"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 R.layout.layout_team_list_item, R.id.teamName, teamNames);
         teamListView.setAdapter(adapter);
+
+        for(int i = 0; i < teamListView.getChildCount(); i++){
+            teamListView.getChildAt(i).
+        }
     }
 }
