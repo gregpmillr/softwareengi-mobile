aabc081dfdbddb2bb48f7940e38af5279e43a451
greg <gregpmiller@gmail.com>
2018-04-01 00:34:24 -0300

    Added javadocs for data models

--- app/src/main/java/com/softengi/mobcomp/softwareengi_mobile/DataModels/TeamDataModel.java
+++ app/src/main/java/com/softengi/mobcomp/softwareengi_mobile/DataModels/TeamDataModel.java
@@ -2,27 +2,54 @@ package com.softengi.mobcomp.softwareengi_mobile.DataModels;
 
 import java.util.ArrayList;
 
+/**
+ * Data model for Teams
+ */
 public class TeamDataModel {
 
-    String mName;
-    String mId;
-    ArrayList<String> mMembers;
+    // member variables
+    private String mName;
+    private String mId;
+    private ArrayList<String> mMembers;
 
+    /**
+     * Constructor for Team data mode
+     * @param name Name of team
+     * @param members Members of this team
+     * @param id Id of this team
+     */
     public TeamDataModel(String name, ArrayList<String> members, String id) {
         mName = name;
         mMembers = members;
         mId = id;
     }
 
+    /**
+     * Constructor for Team data model
+     * @param name Name of team
+     * @param id Id of team
+     */
     public TeamDataModel(String name, String id) {
         mName = name;
         mId = id;
     }
 
+    /**
+     * Get the name of this team
+     * @return the name of this team
+     */
     public String getName() { return mName; }
 
+    /**
+     * Get the Id of this team
+     * @return the Id of this team
+     */
     public String getId() { return mId; }
 
+    /**
+     * Get the members of this team
+     * @return the members of this team
+     */
     public ArrayList<String> getMembers() { return mMembers; }
 
 }
--- app/src/main/java/com/softengi/mobcomp/softwareengi_mobile/DataModels/UserPlanProgressDataModel.java
+++ app/src/main/java/com/softengi/mobcomp/softwareengi_mobile/DataModels/UserPlanProgressDataModel.java
@@ -1,17 +1,34 @@
 package com.softengi.mobcomp.softwareengi_mobile.DataModels;
 
+/**
+ * Data model for a User's progress on a plan
+ */
 public class UserPlanProgressDataModel {
 
+    // member variables
     private String mUsername;
     private String mStepsContributed;
 
+    /**
+     * Constructor for User's Plan Progress data model
+     * @param username Username of user
+     * @param stepsContributed Steps contributed to the plan
+     */
     public UserPlanProgressDataModel(String username, String stepsContributed) {
         mUsername = username;
         mStepsContributed = stepsContributed;
     }
 
+    /**
+     * Get the Username of this user's plan progress
+     * @return the username of the current user
+     */
     public String getUsername() { return mUsername; }
 
+    /**
+     * Get the steps contributed to the plan of this user's progress
+     * @return the steps contributed to the plan of this user's progress
+     */
     public String getStepsContributed() { return mStepsContributed; }
 
 }
